d5d879dc3e5209eaa56c45a3294f58145f1ae319
Marlie Martinez <marlieahue@gmail.com>
2024-07-22 12:27:44 -0400

    finished

--- src/LibraryInterfaceDemo.java
+++ src/LibraryInterfaceDemo.java
@@ -1,8 +1,13 @@
 public class LibraryInterfaceDemo {
     public static void main(String[] args) {
-        KidUser newChildUser = new KidUser(11, "Kids");
-        AdultUser newAdultUser = new AdultUser(28, "Fiction");
+        KidUser newChildUser = new KidUser(10, "Kids");
+        KidUser newChild2 = new KidUser(18, "Fiction");
+        AdultUser newAdultUser = new AdultUser(5, "Kids");
+        AdultUser newAdult2 = new AdultUser(23, "Fiction");
+
         newChildUser.requestBook();
         newAdultUser.requestBook();
+        newChild2.requestBook();
+        newAdult2.requestBook();
     }
 }
